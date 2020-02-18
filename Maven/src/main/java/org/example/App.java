496740e42e48c6c305e8076c7c004a7a93a3cb3b
Amy-Johnston <60958102+Amy-Johnston@users.noreply.github.com>
2020-02-18 20:35:24 +0200

    Update App.java

--- Maven/src/main/java/org/example/App.java
+++ Maven/src/main/java/org/example/App.java
@@ -8,5 +8,6 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.printlin( "I edited, cloned and modified your app Yusrah" );
     }
 }
